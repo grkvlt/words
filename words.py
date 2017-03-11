8d329dfb4524989d954807b5c70c94fe6e72008f
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2017-03-11 19:40:09 +0000

    Add multiple descriptive words

--- words.py
+++ words.py
@@ -9,13 +9,14 @@ people = [
 things = [
     "biscuit", "tree", "pencil sharpener", "marmalade", "toast",
     "fish tank", "bicycle", "spanner", "door", "LEGO man", "book",
-    "cheese", "cat flap", "machine", "space ship", "cake", "apple" ]
+    "cheese", "cat flap", "machine", "space ship", "cake", "apple",
+    "pants", "kitten", "dodecahedron", "picture", "waterfall" ]
 joining = [
     "the", "a", "my", "our" ]
 descriptions = [
     "green", "smelly", "tall", "red", "hollow", "musical", "sleepy",
     "happy", "sad", "exploding", "empty", "silly", "loud", "frightened",
-    "squeaky", "sharp", "bright", "slow" ]
+    "squeaky", "sharp", "bright", "slow", "scary" ]
 actions = [
     "ate", "moved", "threw", "copied", "lifted", "found", "missed",
     "opened", "closed", "sat on", "drove", "pushed", "cleaned",
@@ -29,5 +30,8 @@ thing = choose(things)
 join = choose(joining)
 description = choose(descriptions)
 action = choose(actions)
+
+if random.random() > 0.5:
+    description += " " + choose(descriptions)
 
 print "{} {} {} {} {}".format(person, action, join, description, thing)
