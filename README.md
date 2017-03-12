bbba092c01ee1dc5bc147d496b0f3a129190c167
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2017-03-12 14:56:53 +0000

    Separate story log per invocation

--- README.md
+++ README.md
@@ -16,8 +16,9 @@ Designed for teaching young children about programming.
 
 On OSX with FIGlet installed, try runing `words.sh` to display
 the sentences as a story, and speak them with the speech synthesiser
-at the same time, forever, or until you get bored. The `story.log`
-contains all generated sentences and is appended to by the script.
+at the same time, forever, or until you get bored. The `story.*.log`
+file that is printed when the script exits contains all the generated
+sentences and speakers.
 
 Similarly, `name.sh` provides a _Flash Card_ experience to show
 what learning a language feels like to most people.
--- words.sh
+++ words.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-while true
-do
+story="${TMPDIR}story.$(date +%Y%m%d%H%M%S).log"
+trap "echo ; echo ${story} ; exit 0" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+while true ; do
     clear
-    echo ; tail -6 story.log | cut -d: -f2 ; echo
+    echo
+    test -f ${story} && ( tail -6 ${story} | cut -d: -f2 )
+    echo
     words=$(python words.py)
     person=$(echo "${words}" | cut -d\  -f1)
     case ${person} in
@@ -14,13 +17,12 @@ do
             voice="Oliver" ;;
         Katriona|Wyldstyle)
             voice="Veena" ;;
-        *)
-            words=$(echo "${words}" | sed -E 's/ ((the)|(their)|(this)|(my)|(our)) / /g')
-            voice="Yuri" ;;
+        *)  voice="Yuri"
+            words=$(echo "${words}" | sed -E 's/ ((the)|(their)|(this)|(my)|(our)) / /g') ;;
     esac
     echo "${words}" | tr "a-z" "A-Z" | figlet -c -w $(tput cols)
     echo
-    echo "${voice}: ${words}" >> story.log
+    echo "${voice}: ${words}" >> ${story}
     say -v ${voice} "${words}"
     sleep 5
 done
