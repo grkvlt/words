cae3283494273e30a040f851bafd1514eaa08316
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2017-03-12 11:29:25 +0000

    Configurable voice for names

--- name.sh
+++ name.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-aiff="${TMPDIR}/${RANDOM}.aiff"
-trap "rm ${aiff} ; exit 0" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+VOICE=${1:-Daniel}
+AIFF="${TMPDIR}/${RANDOM}.aiff"
+trap "rm ${AIFF} ; exit 0" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
 while true
 do
     clear
@@ -10,7 +11,8 @@ do
         figlet -w $(tput cols) |
         sed -e "s/^/    /g"
     sleep 1
-    say -v Daniel "${name}"
+    say -v ${VOICE} "${name}"
+    sleep 1
     echo
     orig=$(echo "/ ${name} /" |
         tr "a-z" "A-Z" |
@@ -24,10 +26,10 @@ do
         head -1 |
         tr "A-Z" "a-z")
     echo "        ${orig} (${word})"
-    say -v Daniel --file-format=AIFF -o ${aiff} "${name}"
-    play --no-show-progress "|sox ${aiff} -p speed 0.85 stretch 1.35" \
+    say -v ${VOICE} --file-format=AIFF -o ${AIFF} "${name}"
+    play --no-show-progress "|sox ${AIFF} -p speed 0.85 stretch 1.35" \
         chorus 0.8 0.8 60 0.5 0.5 6 -t
     sleep 2
-    say -v Daniel "${name}"
+    say -v ${VOICE} "${name}"
     sleep 4
 done
