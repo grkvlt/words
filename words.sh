82b3095cc48f1ccc796087b456b07090212a79b2
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2017-03-12 00:03:42 +0000

    Use multiple voices

--- words.sh
+++ words.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# copyright 2017 by andrew donald kennedy
-
-# speak the words
 while true
 do
     words=$(python words.py | tee -a story.log)
@@ -10,6 +7,13 @@ do
     echo ${words} |
         tr "a-z" "A-Z" |
         figlet -c -w $(tput cols)
-    say ${words}
+    voice=$((RANDOM % 5))
+    case ${voice} in
+        0)  say -v Fiona ${words} ;;
+        1)  say -v Oliver ${words} ;;
+        2)  say -v Yuri ${words} ;;
+        3)  say -v Veena ${words} ;;
+        4)  say -v Daniel ${words} ;;
+    esac
     sleep 5
 done
